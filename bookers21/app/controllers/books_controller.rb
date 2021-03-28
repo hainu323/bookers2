07fe645837a8956669c12deea5694798e4faad6a
hainu323 <maxhainu@gmail.com>
2021-03-28 05:13:25 +0000

    finish

---
+++ bookers21/app/controllers/books_controller.rb
@@ -0,0 +1,60 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = 'You have created book successfully.'
+     redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
+  end
+
+  def index
+    @books = Book.all
+    @user = current_user
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @book_new = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    @user = current_user
+    unless @book.user == current_user
+      redirect_to  books_path
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  def update
+    @book = Book.find(params[:id])
+  if@book.update(book_params)
+    flash[:notice] = 'You have updated book successfully.'
+    redirect_to book_path(@book)
+  else
+    render :edit
+  end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
--- bookers21/config/initializers/application_controller_renderer.rb
+++ bookers21/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '124308f04a8472fbe117cb339279cc2df491b45a0b1cced573887455e36a519ddc3c066013053d1c841104e5cdd2764105023f1dc8f18b4d4c22f5b58048df8c'
